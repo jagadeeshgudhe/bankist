d264252479c5a74d806e73080526b92d55ea9d32
Gudhe Jagadeesh <jagadeesh@example.com>
2024-10-08 22:08:05 +0530

    project

---
+++ script.js
@@ -0,0 +1,33 @@
+//customer details
+'use strict';
+//account holder1
+const account1={
+    owner:'Jagadeesh',
+    movements:[20,45,-40,300,-65,-13,7,130],
+    interestRate:0.2,
+    pin:1111,
+};
+
+//account holder2
+const account2={
+    owner:'Laxmi',
+    movements:[50,34,-15,-79,-321,-100,850,-3],
+    interestRate:0.5,
+    pin:2222,
+};
+
+//account holder3
+const account3={
+    owner:'Sanyasi Rao',
+    movements:[20,-20,34,-30,-20,50,40,-46],
+    interestRate:0.35,
+    pin:3333,
+};
+
+//account holder4
+const account4={
+    owner:'Madhu shalini',
+    movements:[20,-20,34,-30,-20,50,40,-46],
+    interestRate:0.35,
+    pin:3333,
+};
